14695ed6622c66d48b31a3681d078a060365a444
Vishnu Prakash <stanbrooks07@gmail.com>
2019-10-31 19:05:50 +0530

    indentation and comments

--- App.js
+++ App.js
@@ -18,16 +18,19 @@ export default class App extends Component {
           			</View>
 					{/* input area */}
 				  	<View style={styles.body}>
-						<View style={{padding:5}}>
+						{/* body area */}
+						<View style={styles.inputPadding}>
 							<TextInput placeholderTextColor="#C0C0C0" style={styles.input} placeholder="Enter a Text"/>
 						</View>
 						
-						<View style={{padding:5}}>
+						<View style={styles.inputPadding}>
 							<TextInput placeholderTextColor="#C0C0C0" style={styles.input} placeholder="Enter a number"/>
 						</View>
 
-						<View style={{padding:5}}>
-							<TouchableOpacity style={styles.submit} onPress={()=>this.props.navigation.navigate("Main")}><Text style={{fontSize:25,textAlign:'center',fontWeight:'bold',color:'#ffffff'}}>Submit</Text></TouchableOpacity>
+						<View style={styles.inputPadding}>
+							<TouchableOpacity style={styles.submit} onPress={()=>this.props.navigation.navigate("Main")}>
+								<Text style={styles.submitText}>Submit</Text>
+							</TouchableOpacity>
 						</View>
           			</View>
         		</View>
@@ -43,55 +46,66 @@ export default class App extends Component {
 
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 8,
-  },
+	container: {
+    	flex: 1,
+    	padding: 8,
+  	},
 
-  divide:{
-    flex:1,
-  },
+  	divide:{
+    	flex:1,
+ 	},
 
-  submit:{
-	borderWidth:2,
-	borderRadius:5,
-	backgroundColor:"#000000",
-	padding:5,
-	minWidth:200,
-	textAlign:'center',
-	//elevation:1,
+  	inputPadding:{
+	  	padding:5,
+  	},
+
+ 	submit:{
+		borderWidth:2,
+		borderRadius:5,
+		backgroundColor:"#000000",
+		padding:5,
+		minWidth:200,
+		textAlign:'center',
+		//elevation:1,
 	},
 
-  body:{
-    flex:1,
-    justifyContent:'center',
-    alignItems:'center',
-  },
+  	submitText:{
+		fontSize:25,
+		textAlign:'center',
+		fontWeight:'bold',
+		color:'#ffffff'
+	},
 
-  header:{
-    flex:1,
-  },
+  	body:{
+		flex:1,
+		justifyContent:'center',
+		alignItems:'center',
+  	},
 
-  headerText:{
-    fontSize:75,
-    color:"#000000",
-    alignSelf:'flex-start',
-  },
+  	header:{
+    	flex:1,
+  	},
 
-  input:{
-    //alignSelf:'stretch',
-    fontSize:25,
-    padding:10,
-    fontFamily:'Roboto',
-    backgroundColor:'transparent',
-    flexDirection:'column',
-    alignSelf:'stretch',
-    textAlign:'center',
-    minWidth:300,
-    borderColor:"#000000",
-    borderWidth:2,
-    // borderRadius:10,
-    elevation:1,
-   // flex: 1,
-},
+  	headerText:{
+		fontSize:75,
+		color:"#000000",
+		alignSelf:'flex-start',
+  	},
+
+  	input:{
+		//alignSelf:'stretch',
+		fontSize:25,
+		padding:10,
+		fontFamily:'Roboto',
+		backgroundColor:'transparent',
+		flexDirection:'column',
+		alignSelf:'stretch',
+		textAlign:'center',
+		minWidth:300,
+		borderColor:"#000000",
+		borderWidth:2,
+		// borderRadius:10,
+		elevation:1,
+		// flex: 1,
+	},
 })
