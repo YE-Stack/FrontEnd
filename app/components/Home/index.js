d63b76a8c633bf5b5ef9b04647f3765f139e2049
Vishnu Prakash <stanbrooks07@gmail.com>
2019-10-31 20:55:06 +0530

    data sending part

--- app/components/Home/index.js
+++ app/components/Home/index.js
@@ -1,13 +1,8 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet,TextInput, TouchableOpacity, ImageBackground, KeyboardAvoidingView } from 'react-native';
 import styles from './styles'
-<<<<<<< HEAD
 import img from '../assets/background.jpg'
-=======
-// import * as firebase from 'firebase'
 import firebase from '../Config/config.js'
-import img from '../Images/background.jpg'
->>>>>>> 7cbf7f79707856a692e8f9322247cc8ca83006f9
 
 export default class Home extends Component {
 	constructor(props) {
@@ -30,7 +25,6 @@ export default class Home extends Component {
 
 	render() {
     	return (
-<<<<<<< HEAD
             <KeyboardAvoidingView>
                 <ImageBackground source={img} style={{width: '100%', height: '100%'}}>
                     <View style={styles.container}>
@@ -44,7 +38,8 @@ export default class Home extends Component {
                                 {/* body area */}
                                 <View style={styles.inputPadding}>
                                     <TextInput placeholderTextColor="#C0C0C0"
-                                        style={styles.input}
+										style={styles.input}
+										onChangeText={(text)=>this.setState({text})}
                                         placeholder="Enter a Text"
                                         keyboardType="default"
                                         />
@@ -53,45 +48,19 @@ export default class Home extends Component {
                                 <View style={styles.inputPadding}>
                                     <TextInput placeholderTextColor="#C0C0C0"
                                         style={styles.input}
-                                        placeholder="Enter a Number"
+										placeholder="Enter a Number"
+										onChangeText={(num)=>this.setState({num})}
                                         keyboardType="number-pad"
                                         />
                                 </View>
 
                                 <View style={styles.inputPadding}>
-                                    <TouchableOpacity style={styles.submit}>
+                                    <TouchableOpacity style={styles.submit} onPress={this.writeData}>
                                         <Text style={styles.submitText}>Submit</Text>
                                     </TouchableOpacity>
                                 </View>
                             </View>
                         </View>
-=======
-			<ImageBackground source={img} style={{width: '100%', height: '100%'}}>
-				<View style={styles.container}>
-					<View style={styles.divide}>
-						{/* header */}
-						{/* <View style={styles.header}>
-							<Text style={styles.headerText}>Input</Text>
-						</View> */}
-						{/* input area */}
-						<View style={styles.body}>
-							{/* body area */}
-							<View style={styles.inputPadding}>
-								<TextInput placeholderTextColor="#C0C0C0" onChangeText={(text)=>this.setState({text})} style={styles.input} placeholder="Enter a Text"/>
-							</View>
-							
-							<View style={styles.inputPadding}>
-								<TextInput placeholderTextColor="#C0C0C0" onChangeText={(num)=>this.setState({num})} style={styles.input} placeholder="Enter a number"/>
-							</View>
-
-							<View style={styles.inputPadding}>
-								<TouchableOpacity style={styles.submit} onPress={this.writeData}>
-									<Text style={styles.submitText}>Submit</Text>
-								</TouchableOpacity>
-							</View>
-						</View>
-					</View>
->>>>>>> 7cbf7f79707856a692e8f9322247cc8ca83006f9
 
                         <View style={styles.divide}>
                             {/* Space for output */}
