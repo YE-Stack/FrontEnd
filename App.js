6b444a6786f1980dcd28e0a2f6dc8d4afefcc598
Jacob Abraham <jacob13400@cet.ac.in>
2019-10-31 20:45:33 +0530

    Icon

--- App.js
+++ App.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet,TextInput, TouchableOpacity } from 'react-native';
 import Home from './app/components/Home'
+import firebase from 'firebase';
 
+var config = {
+    databaseURL: "https://yestack1.firebaseio.com",
+    projectId: "yestack1",
+};
+firebase.initializeApp(config);
+if (!firebase.apps.length) {
+    firebase.initializeApp(config);
+}
 export default Home
 
--- app/components/Home/index.js
+++ app/components/Home/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet,TextInput, TouchableOpacity, ImageBackground } from 'react-native';
+import { View, Text, StyleSheet,TextInput, TouchableOpacity, ImageBackground, KeyboardAvoidingView } from 'react-native';
 import styles from './styles'
-import img from '../Images/background.jpg'
+import img from '../assets/background.jpg'
 
 export default class Home extends Component {
 	constructor(props) {
@@ -12,38 +12,48 @@ export default class Home extends Component {
 
 	render() {
     	return (
-			<ImageBackground source={img} style={{width: '100%', height: '100%'}}>
-				<View style={styles.container}>
-					<View style={styles.divide}>
-						{/* header */}
-						{/* <View style={styles.header}>
-							<Text style={styles.headerText}>Input</Text>
-						</View> */}
-						{/* input area */}
-						<View style={styles.body}>
-							{/* body area */}
-							<View style={styles.inputPadding}>
-								<TextInput placeholderTextColor="#C0C0C0" style={styles.input} placeholder="Enter a Text"/>
-							</View>
-							
-							<View style={styles.inputPadding}>
-								<TextInput placeholderTextColor="#C0C0C0" style={styles.input} placeholder="Enter a number"/>
-							</View>
+            <KeyboardAvoidingView>
+                <ImageBackground source={img} style={{width: '100%', height: '100%'}}>
+                    <View style={styles.container}>
+                        <View style={styles.divide}>
+                            {/* header */}
+                            {/* <View style={styles.header}>
+                                <Text style={styles.headerText}>Input</Text>
+                            </View> */}
+                            {/* input area */}
+                            <View style={styles.body}>
+                                {/* body area */}
+                                <View style={styles.inputPadding}>
+                                    <TextInput placeholderTextColor="#C0C0C0"
+                                        style={styles.input}
+                                        placeholder="Enter a Text"
+                                        keyboardType="default"
+                                        />
+                                </View>
+                                
+                                <View style={styles.inputPadding}>
+                                    <TextInput placeholderTextColor="#C0C0C0"
+                                        style={styles.input}
+                                        placeholder="Enter a Number"
+                                        keyboardType="number-pad"
+                                        />
+                                </View>
 
-							<View style={styles.inputPadding}>
-								<TouchableOpacity style={styles.submit} onPress={()=>this.props.navigation.navigate("Main")}>
-									<Text style={styles.submitText}>Submit</Text>
-								</TouchableOpacity>
-							</View>
-						</View>
-					</View>
+                                <View style={styles.inputPadding}>
+                                    <TouchableOpacity style={styles.submit}>
+                                        <Text style={styles.submitText}>Submit</Text>
+                                    </TouchableOpacity>
+                                </View>
+                            </View>
+                        </View>
 
-					<View style={styles.divide}>
-						{/* Space for output */}
-						<Text>Space for output</Text>
-					</View>
-				</View>
-			</ImageBackground>	
+                        <View style={styles.divide}>
+                            {/* Space for output */}
+                            <Text>Space for output</Text>
+                        </View>
+                    </View>
+                </ImageBackground>	
+            </KeyboardAvoidingView>
     );
   }
 }
